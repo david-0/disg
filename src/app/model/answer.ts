86bfd258e646a61184511bbdf5875377732a86df
David Leuenberger <david.leuenberger@hsr.ch>
2017-10-28 10:11:31 +0200

    make more flexible

--- src/app/model/answer.ts
+++ src/app/model/answer.ts
@@ -1,7 +1,5 @@
 export class Answer {
-  constructor(public questionId: number, public best: number,
-              public middle1: number,
-              public middle2: number,
-              public worst: number) {
+  constructor(public questionId: number,
+              public priorities: number[]) {
   }
 }
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,6 +1,12 @@
 import {
-  Component, OnInit, ElementRef, EventEmitter, Output, Input, ViewChild, Renderer2,
-  AfterContentChecked, DoCheck, AfterViewChecked
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  Input,
+  ViewChild,
+  Renderer2,
+  AfterViewChecked
 } from '@angular/core';
 import {Answer} from '../model/answer';
 import {Question} from '../model/question';
@@ -25,102 +31,69 @@ export class QuestionComponent implements AfterViewChecked {
 
   change(type: string, number: string) {
     if (type === 'upup') {
-      this.answer.best = +number;
-      if (this.answer.best === this.answer.middle1) {
-        this.answer.middle1 = -1;
-      }
-      if (this.answer.best === this.answer.middle2) {
-        this.answer.middle2 = -1;
-      }
-      if (this.answer.best === this.answer.worst) {
-        this.answer.worst = -1;
-      }
+      this.updatePriorities(0, +number);
+    } else if (type === 'up') {
+      this.updatePriorities(1, +number);
+    } else if (type === 'down') {
+      this.updatePriorities(2, +number);
+    } else if (type === 'downdown') {
+      this.updatePriorities(3, +number);
     }
-    if (type === 'up') {
-      this.answer.middle1 = +number;
-      if (this.answer.middle1 === this.answer.best) {
-        this.answer.best = -1;
-      }
-      if (this.answer.middle1 === this.answer.middle2) {
-        this.answer.middle2 = -1;
-      }
-      if (this.answer.middle1 === this.answer.worst) {
-        this.answer.worst = -1;
-      }
-    }
-    if (type === 'down') {
-      this.answer.middle2 = +number;
-      if (this.answer.middle2 === this.answer.best) {
-        this.answer.best = -1;
-      }
-      if (this.answer.middle2 === this.answer.middle1) {
-        this.answer.middle1 = -1;
-      }
-      if (this.answer.middle2 === this.answer.worst) {
-        this.answer.worst = -1;
-      }
-    }
-    if (type === 'downdown') {
-      this.answer.worst = +number;
-      if (this.answer.worst === this.answer.best) {
-        this.answer.best = -1;
-      }
-      if (this.answer.worst === this.answer.middle1) {
-        this.answer.middle1 = -1;
-      }
-      if (this.answer.worst === this.answer.middle2) {
-        this.answer.middle2 = -1;
-      }
-    }
+    this.updateLastPriority();
     this.updateView();
     this.onAnswerChange.emit(this.answer);
     console.log(this.answer);
   }
 
+  private updatePriorities(prio: number, statementNr: number) {
+    this.answer.priorities[prio] = +statementNr;
+    for (let i = 0; i < this.answer.priorities.length; i++) {
+      if (i !== prio && this.answer.priorities[i] === statementNr) {
+        this.answer.priorities[i] = -1;
+      }
+    }
+  }
+
+  private updateLastPriority() {
+    if (this.answer.priorities.filter(p => p === -1).length === 1) {
+      const missingPos = this.findMissingPosition();
+      if (missingPos != null) {
+        this.answer.priorities[missingPos] = this.findMissingNumber();
+      }
+    }
+  }
+
+  private findMissingPosition(): number {
+    for (let i = 0; i < this.answer.priorities.length; i++) {
+      if (this.answer.priorities[i] === -1) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+  private findMissingNumber(): number {
+    for (let i = 0; i < this.answer.priorities.length; i++) {
+      if (this.answer.priorities.findIndex(p => p === i) === -1) {
+        return i;
+      }
+    }
+    return 0;
+  }
+
   private updateView() {
+    const cssClasses = ['best', 'middleUp', 'middleDown', 'worst'];
     const images = this.group.nativeElement.getElementsByTagName('svg');
     const spans = this.group.nativeElement.getElementsByTagName('span');
     for (let i = 0; i < this.question.statements.length; i++) {
-      const imageUpUp = images[i * 4];
-      const imageUp = images[i * 4 + 1];
-      const imageDown = images[i * 4 + 2];
-      const imageDownDown = images[i * 4 + 3];
-      const span = spans[i];
-      if (this.answer.best === i) {
-        this.renderer.addClass(imageUpUp, 'best');
-        this.renderer.removeClass(imageUp, 'middleUp');
-        this.renderer.removeClass(imageDown, 'middleDown');
-        this.renderer.removeClass(imageDownDown, 'worst');
-        this.renderer.addClass(span, 'best');
-        this.renderer.removeClass(span, 'worst');
-      } else if (this.answer.middle1 === i) {
-        this.renderer.removeClass(imageUpUp, 'best');
-        this.renderer.addClass(imageUp, 'middleUp');
-        this.renderer.removeClass(imageDown, 'middleDown');
-        this.renderer.removeClass(imageDownDown, 'worst');
-        this.renderer.removeClass(span, 'best');
-        this.renderer.removeClass(span, 'worst');
-      } else if (this.answer.middle2 === i) {
-        this.renderer.removeClass(imageUpUp, 'best');
-        this.renderer.removeClass(imageUp, 'middleUp');
-        this.renderer.addClass(imageDown, 'middleDown');
-        this.renderer.removeClass(imageDownDown, 'worst');
-        this.renderer.removeClass(span, 'best');
-        this.renderer.removeClass(span, 'worst');
-      } else if (this.answer.worst === i) {
-        this.renderer.removeClass(imageUpUp, 'best');
-        this.renderer.removeClass(imageUp, 'middleUp');
-        this.renderer.removeClass(imageDown, 'middleDown');
-        this.renderer.addClass(imageDownDown, 'worst');
-        this.renderer.removeClass(span, 'best');
-        this.renderer.addClass(span, 'worst');
-      } else {
-        this.renderer.removeClass(imageUpUp, 'best');
-        this.renderer.removeClass(imageUp, 'middleUp');
-        this.renderer.removeClass(imageDown, 'middleDown');
-        this.renderer.removeClass(imageDownDown, 'worst');
-        this.renderer.removeClass(span, 'best');
-        this.renderer.removeClass(span, 'worst');
+      for (let j = 0; j < this.answer.priorities.length; j++) {
+        if (this.answer.priorities[j] === i) {
+          this.renderer.addClass(images[i * 4 + j], cssClasses[j]);
+          this.renderer.addClass(spans[i], cssClasses[j]);
+        } else {
+          this.renderer.removeClass(images[i * 4 + j], cssClasses[j]);
+          this.renderer.removeClass(spans[i], cssClasses[j]);
+        }
       }
     }
   }
