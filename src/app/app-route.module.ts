44c5765fa27fd7dba29e43b0186be6d1c448505c
David Leuenberger <david.leuenberger@hsr.ch>
2017-11-06 08:28:08 +0100

    add routes

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -3,12 +3,20 @@ import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {EvaluationComponent} from './evaluation/evaluation.component';
 import {SelfAssessmentComponent} from './self-assessment/self-assessment.component';
+import {DetailsDComponent} from './extended/details-d/details-d.component';
+import {DetailsGComponent} from './extended/details-g/details-g.component';
+import {DetailsSComponent} from './extended/details-s/details-s.component';
+import {DetailsIComponent} from './extended/details-i/details-i.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: '9382', component: SelfAssessmentComponent},
   {path: 'evaluation', component: EvaluationComponent},
+  {path: '1720', component: DetailsDComponent},
+  {path: '6288', component: DetailsIComponent},
+  {path: '8411', component: DetailsSComponent},
+  {path: '4343', component: DetailsGComponent},
 ];
 
 @NgModule({
